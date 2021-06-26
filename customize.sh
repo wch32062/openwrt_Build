228fd434538721e12fa5017b418cb2fb73ed644e
wch32062 <86425947+wch32062@users.noreply.github.com>
2021-06-26 09:52:07 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,7 +6,7 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.3.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.37.1/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Custom settings
 #cp -f package/litte/default-settings package/lean/default-settings/files/zzz-default-settings
